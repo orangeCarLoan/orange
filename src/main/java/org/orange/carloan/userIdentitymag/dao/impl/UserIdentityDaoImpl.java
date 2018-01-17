c25f0e2aee812d0516808b7a1a94f8130d5be8ab
Administrator <Administrator@172.20.10.2>
2018-01-17 14:33:34 +0800

    2.6.1userIdentityService

--- src/main/java/org/orange/carloan/userIdentitymag/dao/impl/UserIdentityDaoImpl.java
+++ src/main/java/org/orange/carloan/userIdentitymag/dao/impl/UserIdentityDaoImpl.java
@@ -10,12 +10,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserIdentityDaoImpl implements IUserIdentityDao {
 	
-	@Resource UserIdentityMapper uim;
+	@Resource
+	private UserIdentityMapper uim;
 	 
 	@Override
 	public UserIdentityBean findUserIdentityByUserIdentityId(int id) {
 		// TODO Auto-generated method stub
 		return uim.findUserIdentityByUserIdentityId(id);
+	}
+
+	@Override
+	public UserIdentityBean findUserIdentityByIdentity(String identity) {
+		// TODO Auto-generated method stub
+		return uim.findUserIdentityByIdentity(identity);
 	}
 
 }
